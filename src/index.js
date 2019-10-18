4f838cd3007cd12f9b210b8fb825432cf66672e8
timzprof <timilehin.olumofin@gmail.com>
2019-10-18 16:09:47 +0100

    Rearranged CORS headers

--- src/index.js
+++ src/index.js
@@ -54,8 +54,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Enable CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, X-Access-Token, Authorization',
