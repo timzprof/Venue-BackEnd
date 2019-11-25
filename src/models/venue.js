7049b429550ebf78f4818b3e6e97cf42a47c38fe
timzprof <timilehin.olumofin@gmail.com>
2019-11-25 23:15:23 +0100

    Debugging

--- src/models/venue.js
+++ src/models/venue.js
@@ -8,43 +8,43 @@
  * @returns {Object} SequelizeModel
  */
 export default ({Sequelize, db, User, Resource}) => {
-	const Venue = db.define("venue", {
-		id: {
-			type: Sequelize.INTEGER,
-			autoIncrement: true,
-			allowNull: false,
-			primaryKey: true
-		},
-		title: {
-			type: Sequelize.STRING,
-			allowNull: false
-		},
-		address: {
-			type: Sequelize.STRING,
-			allowNull: false
-		},
-		capacity: {
-			type: Sequelize.INTEGER,
-			allowNull: false
-		},
-		featureImage: {
-			type: Sequelize.STRING,
-			allowNull: false
-		},
-		otherImages: {
-			type: Sequelize.ARRAY(Sequelize.STRING),
-			allowNull: false
-		},
-		timeAllowed: {
-			type: Sequelize.ARRAY(Sequelize.STRING),
-			allowNull: false
-		}
-	});
-	Venue.belongsTo(User, {
-		as: "admin",
-		constraints: true,
-		onDelete: "CASCADE"
-	});
-	Venue.hasMany(Resource, {constraints: true, onDelete: "CASCADE"});
-	return Venue;
+  const Venue = db.define('venue', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    capacity: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    featureImage: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    otherImages: {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      allowNull: false,
+    },
+    timeAllowed: {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      allowNull: false,
+    },
+  });
+  Venue.belongsTo(User, {
+    as: 'admin',
+    constraints: true,
+    onDelete: 'CASCADE',
+  });
+  Venue.hasMany(Resource, {constraints: true, onDelete: 'CASCADE'});
+  return Venue;
 };
