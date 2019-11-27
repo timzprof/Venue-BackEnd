081d8392aa6d03995e7d4c0ed5d6e1e543257d35
timzprof <timilehin.olumofin@gmail.com>
2019-11-27 15:17:54 +0100

    Added PATCH to CORS Setup

--- src/index.js
+++ src/index.js
@@ -101,7 +101,7 @@ app.use((req, res, next) => {
 	debugLogger(`Request params: ${prettyStringify(req.params)}`);
 	debugLogger(`Request headers: ${prettyStringify(headers)}`);
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+	res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept, X-Access-Token, Authorization"
